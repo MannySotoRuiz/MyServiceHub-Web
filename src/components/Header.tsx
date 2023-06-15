0201d866b1bc81fa25a391511d67344411b051d7
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-06-15 17:49:32 -0400

    made header responsive with mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
 
 const Header = () => {
+  const [mobileMenuOpened, setMobileMenuOpened] = useState<boolean>(false);
+
   return (
     <section className="text-white">
       <div className="flex gap-y-[32px] flex-wrap items-center justify-between p-[24px] py-[16px] text-[rgba(255,255,255,0.78)] innerWidth">
@@ -11,13 +16,36 @@ const Header = () => {
           width={100}
           height={50}
         />
-        <div className="flex gap-y-[32px] justify-center items-center flex-wrap gap-[32px]">
+        <div className="hidden tablet:flex gap-y-[32px] justify-center items-center flex-wrap gap-[32px]">
           <a href="">Residencies</a>
+          <a href="">Our Values</a>
           <a href="">Contact Us</a>
           <a href="">Get Started</a>
           <button className="button">
             <a href="">Contact</a>
           </button>
+        </div>
+
+        <div
+          className={`text-[#131110] font-medium absolute top-[3rem] right-[4rem] bg-[white] ${
+            mobileMenuOpened ? "flex" : "hidden"
+          } tablet:hidden flex-col gap-y-[32px] justify-center items-start flex-wrap gap-[32px] p-[48px] rounded-[10px] shadow-[0px,4px,4px,rgba(0,0,0,0.05)] z-50 transition`}
+        >
+          <a href="">Residencies</a>
+          <a href="">Our Values</a>
+          <a href="">Contact Us</a>
+          <a href="">Get Started</a>
+          <button className="button">
+            <a href="">Contact</a>
+          </button>
+        </div>
+
+        {/* menu header */}
+        <div
+          onClick={() => setMobileMenuOpened((prev) => !prev)}
+          className="menu-icon tablet:hidden"
+        >
+          <BiMenuAltRight size={30} />
         </div>
       </div>
     </section>
